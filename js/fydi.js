aa515bd3fd47430e12e5dbb721c948f2ebaa94da
JoymeFashion <137570945+JoymeFashion@users.noreply.github.com>
2023-08-06 12:46:40 -0700

    form validation and sweetalert

--- js/fydi.js
+++ js/fydi.js
@@ -2,14 +2,62 @@
 /*
   parameter: type: string (element id)
 */
-var fix_message = function(ele) {
+var fix_message = function (ele) {
   window.onscroll = function () {
-  const messageContainer = document.getElementById(ele);
-  const scrollDistance = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrollDistance > messageContainer.offsetTop) {
+    const messageContainer = document.getElementById(ele);
+    const scrollDistance = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollDistance > messageContainer.offsetTop) {
       messageContainer.classList.add('sticky-message');
-  } else {
+    } else {
       messageContainer.classList.remove('sticky-message');
+    }
   }
-}
 };
+
+// validate form information before submit
+var validate_form = function (ele) {
+  var submit_btns = document.getElementsByClassName(ele);
+  console.log(submit_btns.length);
+  $(submit_btns).each(function () {
+    $(this).on("click", function () {
+      var contact_form = $(this).closest("form");
+      var name = $(contact_form).find(".cs-name").val();
+      var email = $(contact_form).find(".cs-email").val();
+      var phone = $(contact_form).find(".cs-phone").val();
+      var message = "";
+      var alert_msg = "";
+      var info = "";
+      if (name == "") {
+        alert_msg += "Name ";
+      }
+      if (email == "") {
+        alert_msg += "Email ";
+      }
+      if (phone == "") {
+        alert_msg += "Phone ";
+      }
+      if ($(contact_form).find(".cs-message").length>0) {
+        message = $(contact_form).find(".cs-message").val();
+      } else {
+        message = "";
+      }
+      if (alert_msg != "") {
+        Swal.fire({
+          title: 'Error!',
+          text: alert_msg + "is required.",
+          icon: 'error',
+          confirmButtonText: 'Cool'
+        });
+      } else {
+        info = {
+          "name": name,
+          "email": email,
+          "phone": phone,
+          "message": message
+        }
+        console.log(info);
+        // submit info to backend..
+      }
+    });
+  });
+}
